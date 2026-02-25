e0b939937cdcd3a18749637d9d20055be26adede
Harshada291004 <godseharshada05@gmail.com>
2026-02-25 16:39:08 +0530

    Fixed spacy model deployment issue

--- src/extract_info.py
+++ src/extract_info.py
@@ -1,7 +1,12 @@
 import re
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 # ---------------- NAME ----------------
 def extract_name(text):
